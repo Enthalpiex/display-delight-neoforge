b39b4ea5fdacf4163e558e62da0ce173b95a6e74
enthalpiex <enthalpiex@outlook.com>
2025-12-09 18:29:57 -0600

    Update

--- src/main/java/com/jkvin114/displaydelight/init/DisplayProperties.java
+++
@@ -1,7 +0,0 @@
-package com.jkvin114.displaydelight.init;
-
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-
-public class DisplayProperties {
-    public static final IntegerProperty STACKS = IntegerProperty.create("stacks", 1, 6);
-}
